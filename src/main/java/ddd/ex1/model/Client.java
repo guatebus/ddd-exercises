811efb52c746fb349803333e8d343d61d3d644a3
Alejandro Bustamante <alejandro.bustamante@scmspain.com>
2017-05-05 11:36:35 +0200

    moar

--- src/main/java/ddd/ex1/model/Client.java
+++ src/main/java/ddd/ex1/model/Client.java
@@ -2,9 +2,6 @@ package ddd.ex1.model;
 
 import java.util.UUID;
 
-/**
- * Created by alejandro.bustamante on 05/05/17.
- */
 public class Client {
     private UUID id;
     public Contact contactAgent(Ad ad){
--- src/main/java/ddd/ex1/model/Contact.java
+++ src/main/java/ddd/ex1/model/Contact.java
@@ -2,9 +2,6 @@ package ddd.ex1.model;
 
 import java.util.UUID;
 
-/**
- * Created by alejandro.bustamante on 05/05/17.
- */
 public class Contact {
     public Contact(UUID id, UUID clientId) {
     }
